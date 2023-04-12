5d774f8c271acc10c92ff98b25292eaedac71bff
Vanals <marco91vanali@gmail.com>
2023-04-12 13:59:41 +0100

    update package.swift

--- Package.swift
+++ Package.swift
@@ -2,13 +2,10 @@
 import PackageDescription
 
 let package = Package(
-  name: "newskit-themes",
-  dependencies: [
-    .package(url: "https://github.com/Vanals/swift-package-themes.git", branch: "main"),
-  ],
-  targets: [
-    .target(name: "MyTool", dependencies: [
-      .product(name: "SwiftSyntax", package: "swift-syntax"),
-    ]),
-  ]
+    name: "newskit-themes",
+    products: [
+        .library(
+            name: "newskit-themes",
+            targets: []),
+    ]
 )
